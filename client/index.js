8e646f9b9434d72128af5a999650fd4271495ed2
Spinaldash <blueavenger@gmail.com>
2015-04-16 17:39:19 -0700

    catching up to class

--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+/* global Firebase: true */
 'use strict';
 
 var root, users, me;
@@ -5,16 +6,35 @@ var root, users, me;
 $(document).ready(init);
 
 function init(){
-  root = new Firebase('https://zelda-chyld.firebaseio.com/');
+  root = new Firebase('https://mj-firebasegame.firebaseio.com/');
   users = root.child('users');
   $('#create-user').click(createUser);
+  $('#login-user').click(loginUser);
   users.on('child_added', userAdded);
 }
 
 function userAdded(snapshot){
 }
 
+function loginUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+}
+
+
 function createUser(){
+  console.log('running createUser');
   var email = $('#email').val();
   var password = $('#password').val();
 
